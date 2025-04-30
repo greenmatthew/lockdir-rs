81103f02cfa0e8b6b40f626ea6aeab72bd381ca9
Matthew Green <greenmatthew07@gmail.com>
2025-04-30 12:34:13 -0500

    Setting up basic main.rs to have access to help command and license arg.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use clap::{Command, Arg};
+mod commands;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const LICENSE: &str = include_str!("../LICENSE");
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("lockdir")
+        .version(VERSION)
+        .about("File-based directory locking utility")
+        .arg_required_else_help(true)
+        .arg(
+            Arg::new("license")
+                .long("license")
+                .help("Display the license information")
+                .action(clap::ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    if matches.get_flag("license") {
+        println!("{LICENSE}");
+        return;
+    }
+
+    match matches.subcommand() {
+        _ => {
+            panic!("Unexpected command - This should not be reached due to arg_required_else_help(true)");
+        }
+    }
 }
